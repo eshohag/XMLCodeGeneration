203e320dc8c3847117bf858e564d41c93437b131
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-10-06 13:32:15 +0600

    Add project files.

---
+++ XMLCodeGeneration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLCodeGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var soapNs = @"http://schemas.xmlsoap.org/soap/envelope/";
+            var v1 = @"http://BaNCS.TCS.com/webservice/CreateCustomerInterface/v1";
+            var v2 = @"http://TCS.BANCS.Adapter/BANCSSchema";
+            string prefix = "ban";
+
+            var doc = new XmlDocument();
+            var root = doc.AppendChild(doc.CreateElement("", "Envelope", soapNs));
+            var body = root.AppendChild(doc.CreateElement("", "Body", soapNs));
+            var createCustomer = body.AppendChild(doc.CreateElement("", "createCustomer", v1));
+            var custAddRq = createCustomer.AppendChild(doc.CreateElement("", "CustAddRq", ""));
+            var rqHeader = custAddRq.AppendChild(doc.CreateElement("", "RqHeader", v2));
+
+            rqHeader.AppendChild(doc.CreateElement("", "InstNum", v2)).InnerText = "003";
+            rqHeader.AppendChild(doc.CreateElement("", "BrchNum", v2)).InnerText = "0001";
+
+            var data = custAddRq.AppendChild(doc.CreateElement("", "Data", v2));
+            data.AppendChild(doc.CreateElement("", "SSDone", v2)).InnerText = "N";
+
+            var xml = doc.GetOuterXml();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XMLCodeGeneration/XmlSerializationHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace XMLCodeGeneration
+{
+    public static class XmlSerializationHelper
+    {
+        public static string GetOuterXml(this XmlNode node, bool indent = true)
+        {
+            if (node == null)
+                return null;
+            using (var textWriter = new StringWriter())
+            {
+                var settings = new XmlWriterSettings
+                {
+                    Indent = indent,
+                    OmitXmlDeclaration = true,
+                };
+                using (var xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    node.WriteTo(xmlWriter);
+                }
+                return textWriter.ToString();
+            }
+        }
+    }
+
+}
