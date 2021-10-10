b2597345d0029c40da5f8979e9c90a6575d887b5
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-10-10 17:54:48 +0600

    Generate XML

---
+++ XMLCodeGeneration/CustAddRq_Type_Custom.cs
@@ -0,0 +1,155 @@
+﻿namespace XMLCodeGeneration
+{
+    public class CustAddRq_Type_Custom
+    {
+        public CustAddRq_Type_Custom()
+        {
+            RqHeader = new RqHeader_Type_Custom();
+            Data = new CustAddRqData_Type_Custom();
+        }
+
+        public RqHeader_Type_Custom RqHeader { get; set; }
+        public CustAddRqData_Type_Custom Data { get; set; }
+    }
+
+    public class CustAddRqData_Type_Custom
+    {
+        public string NidVrfy { get; set; }
+        public string SSDone { get; set; }
+        public string SelfSPouseParent { get; set; }
+        public string DocObtainAddr { get; set; }
+        public string FormrAddrProof { get; set; }
+        public string AddrProofObtain { get; set; }
+        public string HasAddrBeenVrfy { get; set; }
+        public string HowTheAddrVrfy { get; set; }
+        public string FrillNoFlag { get; set; }
+        public string MaritalStat { get; set; }
+        public string CustLastName { get; set; }
+        public string BengaliName { get; set; }
+        public string SpouseName { get; set; }
+        public string DisblFlag { get; set; }
+        public string Natlty2 { get; set; }
+        public string Natlty3 { get; set; }
+        public string Flag1 { get; set; }
+        public string CustRisk { get; set; }
+        public string ReguLike { get; set; }
+        public string ReguImp { get; set; }
+        public string DevChanl2 { get; set; }
+        public string ProdServ2 { get; set; }
+        public string Designation { get; set; }
+        public string ProdServ1 { get; set; }
+        public string Attr7 { get; set; }
+        public string DevChanl1 { get; set; }
+        public string UsCust { get; set; }
+        public string Usw9form { get; set; }
+        public string Usgreencard { get; set; }
+        public string UsAddrFlag { get; set; }
+        public string UsPwrAttorny { get; set; }
+        public string UsStayed { get; set; }
+        public string UsPaySrc { get; set; }
+        public string CentreCode { get; set; }
+        public string AgntCode { get; set; }
+        public string MoblieEmg { get; set; }
+        public string MadeInIndia { get; set; }
+        public string OfcAddr { get; set; }
+        public string RelnEmg { get; set; }
+        public string CountryEmg { get; set; }
+        public string Div { get; set; }
+        public string DisttCode { get; set; }
+        public string FinclInclsn { get; set; }
+        public string IssueAuthty { get; set; }
+        public string IdIssuePlace { get; set; }
+        public string IdIssueDt { get; set; }
+        public string ApprvlOfSenior { get; set; }
+        public string IsTheFamily { get; set; }
+        public string GreetingReqd { get; set; }
+        public string ProfCentr { get; set; }
+        public string LckrHldr { get; set; }
+        public string Caste { get; set; }
+        public string ProofAddrTyp { get; set; }
+        public string RiskScoreMat { get; set; }
+        public string NameEmg { get; set; }
+        public string SubOfc2 { get; set; }
+        public string Thana1 { get; set; }
+        public string SubOfc1 { get; set; }
+        public string Thana2 { get; set; }
+        public string CityCode1 { get; set; }
+        public string CityCode { get; set; }
+        public string StateCode1 { get; set; }
+        public string State { get; set; }
+        public string BussPhnNum { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryCode2 { get; set; }
+        public string NrstLndMark { get; set; }
+        public string IsTheCust { get; set; }
+        public string NameAsProof { get; set; }
+        public string PolitclEP { get; set; }
+        public string MnthlyIncome { get; set; }
+        public string CustTyp { get; set; }
+        public string TitleCode { get; set; }
+        public string CustFirstName { get; set; }
+        public string Addr11 { get; set; }
+        public string Addr12 { get; set; }
+        public string Addr10 { get; set; }
+        public string Addr9 { get; set; }
+        public string Addr1 { get; set; }
+        public string Addr2 { get; set; }
+        public string CountryCode1 { get; set; }
+        public string Natlty { get; set; }
+        public string AcctNum1 { get; set; }
+        public string IntroName { get; set; }
+        public string Relcusm { get; set; }
+        public string NumAcct { get; set; }
+        public string MobileNum235 { get; set; }
+        public string AcctBrnch { get; set; }
+        public string LangCode { get; set; }
+        public string PpVrfy { get; set; }
+        public string IdNum1 { get; set; }
+        public string IdTyp1 { get; set; }
+        public string IdExpryDt { get; set; }
+        public string DmstcRisk { get; set; }
+        public string CrossBrdrRisk { get; set; }
+        public string SegCode { get; set; }
+        public string HomeBrchNum { get; set; }
+        public string CountryRisk { get; set; }
+        public string GrpCode { get; set; }
+        public string FatherName { get; set; }
+        public string MotherMdnName { get; set; }
+        public string DtOfBirth { get; set; }
+        public string ResdncyCode { get; set; }
+        public string OrigCode { get; set; }
+        public string StrtCode { get; set; }
+        public string GndrCode { get; set; }
+        public string CustGrp { get; set; }
+        public string CountryOfRsdnc { get; set; }
+        public string RevaltnFlag { get; set; }
+        public string OccupCode { get; set; }
+        public string PrevOccupCode { get; set; }
+        public string IncomeRange { get; set; }
+        public string CustRiskImp { get; set; }
+        public string CountryOfBirth { get; set; }
+        public string CountryOfTaxRsdnc { get; set; }
+        public string CounRiskImp { get; set; }
+        public string CaptureProprietor { get; set; }
+        public string PrimTyp1 { get; set; }
+        public string SecndTyp1 { get; set; }
+        public string PrimTyp2 { get; set; }
+        public string DisttBirth { get; set; }
+        public string SmeTyp { get; set; }
+        public string BankNm { get; set; }
+        public string CorrAddrLan { get; set; }
+        public string CorrAddrMobile { get; set; }
+        public string EmergencyContactMobile { get; set; }
+    }
+    public class RqHeader_Type_Custom
+    {
+        public string InstNum { get; set; }
+        public string TellerNum { get; set; }
+        public string BrchNum { get; set; }
+        public string UUIDSource { get; set; }
+        public string Flag5 { get; set; }
+        public string Flag4 { get; set; }
+        public string UUIDNUM { get; set; }
+        public string UUIDSeqNo { get; set; }
+    }
+}
--- XMLCodeGeneration/Program.cs
+++ XMLCodeGeneration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Xml;
 
 namespace XMLCodeGeneration
@@ -10,7 +11,7 @@ namespace XMLCodeGeneration
             var soapNs = @"http://schemas.xmlsoap.org/soap/envelope/";
             var v1 = @"http://BaNCS.TCS.com/webservice/CreateCustomerInterface/v1";
             var v2 = @"http://TCS.BANCS.Adapter/BANCSSchema";
-            string prefix = "ban";
+            string prefix = "";
 
             var doc = new XmlDocument();
             var root = doc.AppendChild(doc.CreateElement("", "Envelope", soapNs));
@@ -21,25 +22,174 @@ namespace XMLCodeGeneration
             var rqHeader = custAddRq.AppendChild(doc.CreateElement("", "RqHeader", v2));
             var data = custAddRq.AppendChild(doc.CreateElement("", "Data", v2));
 
-            if (string.IsNullOrWhiteSpace(prefix))
+            #region Object Mapping
+            var aAddCustomer = new CustAddRq_Type_Custom();
+            aAddCustomer.RqHeader = new RqHeader_Type_Custom();
+            aAddCustomer.Data = new CustAddRqData_Type_Custom();
+
+            #region Request_Header Accroding to CreateCustomerInterfaceHttpService
+            aAddCustomer.RqHeader.InstNum = "003";
+            aAddCustomer.RqHeader.BrchNum = "0001";
+            aAddCustomer.RqHeader.TellerNum = "5000055";
+            aAddCustomer.RqHeader.Flag4 = "W";
+            aAddCustomer.RqHeader.Flag5 = "Y";
+            aAddCustomer.RqHeader.UUIDSource = "WS";
+            aAddCustomer.RqHeader.UUIDNUM = "009";
+            aAddCustomer.RqHeader.UUIDSeqNo = "0";
+
+            #endregion
+
+            #region Request_Data Accroding to CreateCustomerInterfaceHttpService
+            aAddCustomer.Data.NidVrfy = "Y";
+            aAddCustomer.Data.SSDone = "N";
+            aAddCustomer.Data.FormrAddrProof = "N";
+            aAddCustomer.Data.AddrProofObtain = "05";
+            aAddCustomer.Data.DocObtainAddr = "05";
+            aAddCustomer.Data.SelfSPouseParent = "N";
+            aAddCustomer.Data.HasAddrBeenVrfy = "Y";
+            aAddCustomer.Data.HowTheAddrVrfy = "1";
+            aAddCustomer.Data.FrillNoFlag = "N";
+            aAddCustomer.Data.MaritalStat = "M";
+            aAddCustomer.Data.CustLastName = "TAZUL";
+            aAddCustomer.Data.BengaliName = "Name IN bangali";
+            aAddCustomer.Data.SpouseName = "Md.Anser Ali";
+            aAddCustomer.Data.DisblFlag = "N";
+            aAddCustomer.Data.Natlty2 = "BD";
+            aAddCustomer.Data.Natlty3 = "BD";
+            //aAddCustomer.Data.Attr7 = "N";
+            aAddCustomer.Data.Attr7 = "01777777777";
+            aAddCustomer.Data.Flag1 = "Y";
+            aAddCustomer.Data.CustRisk = "09";
+            aAddCustomer.Data.ReguLike = "2";
+            aAddCustomer.Data.ReguImp = "3";
+            aAddCustomer.Data.ProdServ1 = "2";
+            aAddCustomer.Data.ProdServ2 = "3";
+            aAddCustomer.Data.DevChanl1 = "2";
+            aAddCustomer.Data.DevChanl2 = "3";
+            aAddCustomer.Data.Designation = "16";
+            aAddCustomer.Data.UsCust = "N";
+            aAddCustomer.Data.Usgreencard = "N";
+            aAddCustomer.Data.Usw9form = "N";
+            aAddCustomer.Data.UsAddrFlag = "N";
+            aAddCustomer.Data.UsPwrAttorny = "N";
+            aAddCustomer.Data.UsStayed = "N";
+            aAddCustomer.Data.UsPaySrc = "N";
+            aAddCustomer.Data.CentreCode = "AB000116";
+            aAddCustomer.Data.AgntCode = "US000726";
+            aAddCustomer.Data.MoblieEmg = "01999999999";
+            aAddCustomer.Data.MadeInIndia = "616";
+            aAddCustomer.Data.OfcAddr = "1209";
+            aAddCustomer.Data.RelnEmg = "04";
+            aAddCustomer.Data.CountryEmg = "BD";
+            aAddCustomer.Data.Div = "060";
+            aAddCustomer.Data.DisttCode = "091";
+            aAddCustomer.Data.FinclInclsn = "N";
+            aAddCustomer.Data.IssueAuthty = "Barishal";
+            aAddCustomer.Data.IdIssuePlace = "Barishal";
+            aAddCustomer.Data.IdIssueDt = "01012008";
+            aAddCustomer.Data.ApprvlOfSenior = "N";
+            aAddCustomer.Data.IsTheFamily = "N";
+            aAddCustomer.Data.GreetingReqd = "0";
+            aAddCustomer.Data.ProfCentr = "00";
+            aAddCustomer.Data.LckrHldr = "";
+            aAddCustomer.Data.Caste = "004";
+            aAddCustomer.Data.ProofAddrTyp = "Present Address";
+            aAddCustomer.Data.RiskScoreMat = "3";
+            aAddCustomer.Data.NameEmg = "NameEmg 007";
+            aAddCustomer.Data.SubOfc2 = "7800";
+            aAddCustomer.Data.SubOfc1 = "1209";
+            aAddCustomer.Data.Thana1 = "616";
+            aAddCustomer.Data.Thana2 = "632";
+            aAddCustomer.Data.CityCode1 = "006";
+            aAddCustomer.Data.CityCode = "026";
+            aAddCustomer.Data.StateCode1 = "010";
+            aAddCustomer.Data.State = "030";
+            aAddCustomer.Data.BussPhnNum = "0188888888888";
+            aAddCustomer.Data.CountryCode = "BD";
+            aAddCustomer.Data.CountryCode2 = "BD";
+            aAddCustomer.Data.NrstLndMark = "Mohammodpur";
+            aAddCustomer.Data.NameAsProof = "NameAsProof";
+            aAddCustomer.Data.IsTheCust = "N";
+            aAddCustomer.Data.PolitclEP = "10";
+            aAddCustomer.Data.MnthlyIncome = "150000";
+            aAddCustomer.Data.CustTyp = "01";
+            aAddCustomer.Data.TitleCode = "02";
+            aAddCustomer.Data.CustFirstName = "Tazul";
+            aAddCustomer.Data.Addr11 = "Address 11";
+            aAddCustomer.Data.Addr12 = "Address 12";
+            aAddCustomer.Data.Addr10 = "Address 10 ";
+            aAddCustomer.Data.Addr9 = "Address 9 need to test";
+            aAddCustomer.Data.Addr1 = "Address 1";
+            aAddCustomer.Data.Addr2 = "Address 2";
+            aAddCustomer.Data.CountryCode1 = "BD";
+            aAddCustomer.Data.Natlty = "BD";
+            aAddCustomer.Data.AcctNum1 = "113039058";
+            aAddCustomer.Data.IntroName = "Mr WAHIDUZZAMAN";
+            aAddCustomer.Data.Relcusm = "06";
+            aAddCustomer.Data.NumAcct = "1130310293845";
+            aAddCustomer.Data.MobileNum235 = "01712671832";
+            aAddCustomer.Data.AcctBrnch = "00113";
+            aAddCustomer.Data.LangCode = "01";
+            aAddCustomer.Data.PpVrfy = "N";
+            aAddCustomer.Data.IdNum1 = "5955963891";
+            aAddCustomer.Data.IdTyp1 = "01";
+            aAddCustomer.Data.IdExpryDt = "";
+            aAddCustomer.Data.DmstcRisk = "ZZ";
+            aAddCustomer.Data.CrossBrdrRisk = "ZZ";
+            aAddCustomer.Data.SegCode = "0001";
+            aAddCustomer.Data.HomeBrchNum = "113";
+            aAddCustomer.Data.CountryRisk = "02";
+            aAddCustomer.Data.GrpCode = "ZZ";
+            aAddCustomer.Data.FatherName = "Farid Ahmad";
+            aAddCustomer.Data.MotherMdnName = "Nazmun nahar";
+            aAddCustomer.Data.DtOfBirth = "25011994";
+            aAddCustomer.Data.ResdncyCode = "1";
+            aAddCustomer.Data.OrigCode = "ZZ";
+            aAddCustomer.Data.StrtCode = "ZZ";
+            aAddCustomer.Data.GndrCode = "M";
+            aAddCustomer.Data.CustGrp = "0103";
+            aAddCustomer.Data.CountryOfRsdnc = "BD";
+            aAddCustomer.Data.RevaltnFlag = "N";
+            aAddCustomer.Data.OccupCode = "F";
+            aAddCustomer.Data.PrevOccupCode = "0025";
+            aAddCustomer.Data.IncomeRange = "01";
+            aAddCustomer.Data.CustRiskImp = "3";
+            aAddCustomer.Data.CountryOfBirth = "BD";
+            aAddCustomer.Data.CountryOfTaxRsdnc = "BD";
+            aAddCustomer.Data.CounRiskImp = "3";
+            aAddCustomer.Data.CaptureProprietor = "N";
+            aAddCustomer.Data.PrimTyp1 = "04";
+            aAddCustomer.Data.SecndTyp1 = "03";
+            aAddCustomer.Data.PrimTyp2 = "915001";
+            aAddCustomer.Data.DisttBirth = "006";
+            aAddCustomer.Data.SmeTyp = "00";
+            aAddCustomer.Data.BankNm = "123";
+            aAddCustomer.Data.CorrAddrLan = "+88";
+            aAddCustomer.Data.CorrAddrMobile = "+88";
+            aAddCustomer.Data.EmergencyContactMobile = "+88";
+            #endregion
+
+            #endregion
+
+            var propertiesRqHeader = typeof(RqHeader_Type_Custom).GetProperties();
+
+            foreach (PropertyInfo property in propertiesRqHeader)
             {
-                rqHeader.AppendChild(doc.CreateElement("", "InstNum", v2)).InnerText = "003";
-                rqHeader.AppendChild(doc.CreateElement("", "BrchNum", v2)).InnerText = "0001";
-                data.AppendChild(doc.CreateElement("", "SSDone", v2)).InnerText = "N";
-
+                var propValue = property.GetValue(aAddCustomer.RqHeader, null);
+                if (propValue != null && !string.IsNullOrWhiteSpace(propValue.ToString()))
+                    rqHeader.AppendChild(doc.CreateElement(string.IsNullOrWhiteSpace(prefix) ? "" : prefix, property.Name, v2)).InnerText = propValue == null ? "" : propValue.ToString();
             }
-            else
+            var propertiesData = typeof(CustAddRqData_Type_Custom).GetProperties();
+            foreach (PropertyInfo property in propertiesData)
             {
-                rqHeader.AppendChild(doc.CreateElement(prefix, "InstNum", v2)).InnerText = "003";
-                rqHeader.AppendChild(doc.CreateElement(prefix, "BrchNum", v2)).InnerText = "0001";
-                data.AppendChild(doc.CreateElement(prefix, "SSDone", v2)).InnerText = "N";
-
+                var propValue = property.GetValue(aAddCustomer.Data, null);
+                if (propValue != null && !string.IsNullOrWhiteSpace(propValue.ToString()))
+                    data.AppendChild(doc.CreateElement(string.IsNullOrWhiteSpace(prefix) ? "" : prefix, property.Name, v2)).InnerText = propValue == null ? "" : propValue.ToString();
             }
-
-
-            var xml = doc.GetOuterXml();
+            var generatedXML = doc.GetOuterXml();
 
             Console.ReadKey();
         }
     }
 }
+
