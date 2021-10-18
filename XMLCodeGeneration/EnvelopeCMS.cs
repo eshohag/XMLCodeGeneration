eae77f3acf9ff7db07cf86a4b0c6adf5cca7d5d9
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-10-18 18:19:18 +0600

    CMS Code

---
+++ XMLCodeGeneration/EnvelopeCMS.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace XMLCodeGeneration
+{
+    // using System.Xml.Serialization;
+    // XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
+    // using (StringReader reader = new StringReader(xml))
+    // {
+    //    var test = (Envelope)serializer.Deserialize(reader);
+    // }
+
+    [XmlRoot(ElementName = "RequestHeader")]
+    public class RequestHeader
+    {
+
+        [XmlElement(ElementName = "Branch")]
+        public int Branch { get; set; }
+
+        [XmlElement(ElementName = "Station")]
+        public int Station { get; set; }
+
+        [XmlElement(ElementName = "RType")]
+        public string RType { get; set; }
+
+        [XmlElement(ElementName = "Cpimode")]
+        public string Cpimode { get; set; }
+
+        [XmlElement(ElementName = "User")]
+        public string User { get; set; }
+
+        [XmlElement(ElementName = "Pwd")]
+        public string Pwd { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Header")]
+    public class Header
+    {
+
+        [XmlElement(ElementName = "RequestHeader")]
+        public RequestHeader RequestHeader { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Command")]
+    public class Command
+    {
+
+        [XmlAttribute(AttributeName = "Action")]
+        public string Action { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ATTRIBUTE")]
+    public class ATTRIBUTE
+    {
+
+        [XmlAttribute(AttributeName = "ID")]
+        public string ID { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CUSTOMATTRIBUTES")]
+    public class CUSTOMATTRIBUTES
+    {
+
+        [XmlElement(ElementName = "ATTRIBUTE")]
+        public List<ATTRIBUTE> ATTRIBUTE { get; set; }
+    }
+
+    [XmlRoot(ElementName = "POSTALADDRESS")]
+    public class POSTALADDRESS
+    {
+
+        [XmlElement(ElementName = "ZIPCODE")]
+        public int ZIPCODE { get; set; }
+
+        [XmlElement(ElementName = "LINE")]
+        public string LINE { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Person")]
+    public class Person
+    {
+
+        [XmlElement(ElementName = "Command")]
+        [XmlAttribute("Action")]
+        public Command Command { get; set; }
+
+        [XmlElement(ElementName = "CUSTOMATTRIBUTES")]
+        public CUSTOMATTRIBUTES CUSTOMATTRIBUTES { get; set; }
+
+        [XmlElement(ElementName = "FirstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement(ElementName = "LastName")]
+        [XmlAttribute("Action")]
+        public string LastName { get; set; }
+
+        [XmlElement(ElementName = "TITLE")]
+        public int TITLE { get; set; }
+
+        [XmlElement(ElementName = "Name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "Sex")]
+        public string Sex { get; set; }
+
+        [XmlElement(ElementName = "BIRTHDATE")]
+        public int BIRTHDATE { get; set; }
+
+        [XmlElement(ElementName = "MOBILEPHONE")]
+        public string MOBILEPHONE { get; set; }
+
+        [XmlElement(ElementName = "CountryCitizen")]
+        public int CountryCitizen { get; set; }
+
+        [XmlElement(ElementName = "POSTALADDRESS")]
+        public POSTALADDRESS POSTALADDRESS { get; set; }
+
+        [XmlElement(ElementName = "BRANCHPART")]
+        public int BRANCHPART { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Request")]
+    public class Request
+    {
+
+        [XmlElement(ElementName = "Person")]
+        public Person Person { get; set; }
+
+        [XmlElement(ElementName = "Request")]
+        public Request Request1 { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Body")]
+    public class Body
+    {
+
+        [XmlElement(ElementName = "Request")]
+        public Request Request { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Envelope")]
+    public class Envelope
+    {
+
+        [XmlElement(ElementName = "Header")]
+        public Header Header { get; set; }
+
+        [XmlElement(ElementName = "Body")]
+        public Body Body { get; set; }
+    }
+
+
+
+}
--- XMLCodeGeneration/Program.cs
+++ XMLCodeGeneration/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 using System.Xml;
 
 namespace XMLCodeGeneration
@@ -8,188 +7,89 @@ namespace XMLCodeGeneration
     {
         static void Main(string[] args)
         {
+
+            #region XMLBasicStructure
             var soapNs = @"http://schemas.xmlsoap.org/soap/envelope/";
-            var v1 = @"http://BaNCS.TCS.com/webservice/CreateCustomerInterface/v1";
+            var cpi = @"http://schemas.compassplus.com/twcms/1.0/omsi.xsd";
+            var v1 = @"http://TCS.BANCS.Adapter/BANCSSchema";
             var v2 = @"http://TCS.BANCS.Adapter/BANCSSchema";
-            string prefix = "";
+            string prefix = "SOAP-ENV";
 
             var doc = new XmlDocument();
-            var root = doc.AppendChild(doc.CreateElement("", "Envelope", soapNs));
-            var body = root.AppendChild(doc.CreateElement("", "Body", soapNs));
-            var createCustomer = body.AppendChild(doc.CreateElement("", "createCustomer", v1));
-            var custAddRq = createCustomer.AppendChild(doc.CreateElement("", "CustAddRq", ""));
+            var root = doc.AppendChild(doc.CreateElement(prefix, "Envelope", soapNs));
+            var header = root.AppendChild(doc.CreateElement(prefix, "Header", soapNs));
+            var requestHeader = header.AppendChild(doc.CreateElement("cpi", "RequestHeader", cpi));
+            requestHeader.AppendChild(doc.CreateElement("", "Branch", "")).InnerText = "1";
+            requestHeader.AppendChild(doc.CreateElement("", "Station", "")).InnerText = "90151";
+            requestHeader.AppendChild(doc.CreateElement("", "RType", "")).InnerText = "Do";
+            requestHeader.AppendChild(doc.CreateElement("", "Cpimode", "")).InnerText = "Sync";
+            requestHeader.AppendChild(doc.CreateElement("", "User", "")).InnerText = "IBOMS";
+            requestHeader.AppendChild(doc.CreateElement("", "Pwd", "")).InnerText = "IBOMS";
 
-            var rqHeader = custAddRq.AppendChild(doc.CreateElement("", "RqHeader", v2));
-            var data = custAddRq.AppendChild(doc.CreateElement("", "Data", v2));
 
-            #region Object Mapping
-            var aAddCustomer = new CustAddRq_Type_Custom();
-            aAddCustomer.RqHeader = new RqHeader_Type_Custom();
-            aAddCustomer.Data = new CustAddRqData_Type_Custom();
+            var body = root.AppendChild(doc.CreateElement(prefix, "Body", soapNs));
+            var request = body.AppendChild(doc.CreateElement("cpi", "Request", cpi));
+            var request2 = request.AppendChild(doc.CreateElement("", "Request", ""));
+            var person = request2.AppendChild(doc.CreateElement("", "Person", ""));
 
-            #region Request_Header Accroding to CreateCustomerInterfaceHttpService
-            aAddCustomer.RqHeader.InstNum = "003";
-            aAddCustomer.RqHeader.BrchNum = "0001";
-            aAddCustomer.RqHeader.TellerNum = "5000055";
-            aAddCustomer.RqHeader.Flag4 = "W";
-            aAddCustomer.RqHeader.Flag5 = "Y";
-            aAddCustomer.RqHeader.UUIDSource = "WS";
-            aAddCustomer.RqHeader.UUIDNUM = "009";
-            aAddCustomer.RqHeader.UUIDSeqNo = "0";
+            XmlElement Command = doc.CreateElement("", "Command", "");
+            Command.SetAttribute("Action", "Create");
+            person.AppendChild(Command);
 
+            var customeAttributes = person.AppendChild(doc.CreateElement("", "CUSTOMATTRIBUTES", ""));
+            XmlElement attributeF = doc.CreateElement("", "ATTRIBUTE", "");
+            attributeF.SetAttribute("ID", "FATHERNAME");
+            customeAttributes.AppendChild(attributeF).InnerText = "Fathers name of customer";
+
+            XmlElement attributeM = doc.CreateElement("", "ATTRIBUTE", "");
+            attributeM.SetAttribute("ID", "MOTHERNAME");
+            customeAttributes.AppendChild(attributeM).InnerText = "MOTHERNAME name of customer";
+
+            person.AppendChild(doc.CreateElement("Name")).InnerText = "MD Shohag Mia";
+            person.AppendChild(doc.CreateElement("LastName")).InnerText = "MD Shohag";
+            person.AppendChild(doc.CreateElement("FirstName")).InnerText = "Mia";
+            person.AppendChild(doc.CreateElement("TITLE")).InnerText = "1";
+            person.AppendChild(doc.CreateElement("Sex")).InnerText = "M";
+            person.AppendChild(doc.CreateElement("BIRTHDATE")).InnerText = "19850315";
+            person.AppendChild(doc.CreateElement("MOBILEPHONE")).InnerText = "880(191)8111460";
+            person.AppendChild(doc.CreateElement("CountryCitizen")).InnerText = "50";
+            person.AppendChild(doc.CreateElement("BRANCHPART")).InnerText = "1";
+
+
+            XmlElement postalAddress = doc.CreateElement("", "POSTALADDRESS", "");
+            postalAddress.SetAttribute("TYPE", "3");
+            var postalInfo = person.AppendChild(postalAddress);
+            postalInfo.AppendChild(doc.CreateElement("ZIPCODE")).InnerText = "1200";
+            postalInfo.AppendChild(doc.CreateElement("LINE")).InnerText = "DHAKA";
+
+            var xml = doc.GetOuterXml();
             #endregion
 
-            #region Request_Data Accroding to CreateCustomerInterfaceHttpService
-            aAddCustomer.Data.NidVrfy = "Y";
-            aAddCustomer.Data.SSDone = "N";
-            aAddCustomer.Data.FormrAddrProof = "N";
-            aAddCustomer.Data.AddrProofObtain = "05";
-            aAddCustomer.Data.DocObtainAddr = "05";
-            aAddCustomer.Data.SelfSPouseParent = "N";
-            aAddCustomer.Data.HasAddrBeenVrfy = "Y";
-            aAddCustomer.Data.HowTheAddrVrfy = "1";
-            aAddCustomer.Data.FrillNoFlag = "N";
-            aAddCustomer.Data.MaritalStat = "M";
-            aAddCustomer.Data.CustLastName = "TAZUL";
-            aAddCustomer.Data.BengaliName = "Name IN bangali";
-            aAddCustomer.Data.SpouseName = "Md.Anser Ali";
-            aAddCustomer.Data.DisblFlag = "N";
-            aAddCustomer.Data.Natlty2 = "BD";
-            aAddCustomer.Data.Natlty3 = "BD";
-            //aAddCustomer.Data.Attr7 = "N";
-            aAddCustomer.Data.Attr7 = "01777777777";
-            aAddCustomer.Data.Flag1 = "Y";
-            aAddCustomer.Data.CustRisk = "09";
-            aAddCustomer.Data.ReguLike = "2";
-            aAddCustomer.Data.ReguImp = "3";
-            aAddCustomer.Data.ProdServ1 = "2";
-            aAddCustomer.Data.ProdServ2 = "3";
-            aAddCustomer.Data.DevChanl1 = "2";
-            aAddCustomer.Data.DevChanl2 = "3";
-            aAddCustomer.Data.Designation = "16";
-            aAddCustomer.Data.UsCust = "N";
-            aAddCustomer.Data.Usgreencard = "N";
-            aAddCustomer.Data.Usw9form = "N";
-            aAddCustomer.Data.UsAddrFlag = "N";
-            aAddCustomer.Data.UsPwrAttorny = "N";
-            aAddCustomer.Data.UsStayed = "N";
-            aAddCustomer.Data.UsPaySrc = "N";
-            aAddCustomer.Data.CentreCode = "AB000116";
-            aAddCustomer.Data.AgntCode = "US000726";
-            aAddCustomer.Data.MoblieEmg = "01999999999";
-            aAddCustomer.Data.MadeInIndia = "616";
-            aAddCustomer.Data.OfcAddr = "1209";
-            aAddCustomer.Data.RelnEmg = "04";
-            aAddCustomer.Data.CountryEmg = "BD";
-            aAddCustomer.Data.Div = "060";
-            aAddCustomer.Data.DisttCode = "091";
-            aAddCustomer.Data.FinclInclsn = "N";
-            aAddCustomer.Data.IssueAuthty = "Barishal";
-            aAddCustomer.Data.IdIssuePlace = "Barishal";
-            aAddCustomer.Data.IdIssueDt = "01012008";
-            aAddCustomer.Data.ApprvlOfSenior = "N";
-            aAddCustomer.Data.IsTheFamily = "N";
-            aAddCustomer.Data.GreetingReqd = "0";
-            aAddCustomer.Data.ProfCentr = "00";
-            aAddCustomer.Data.LckrHldr = "";
-            aAddCustomer.Data.Caste = "004";
-            aAddCustomer.Data.ProofAddrTyp = "Present Address";
-            aAddCustomer.Data.RiskScoreMat = "3";
-            aAddCustomer.Data.NameEmg = "NameEmg 007";
-            aAddCustomer.Data.SubOfc2 = "7800";
-            aAddCustomer.Data.SubOfc1 = "1209";
-            aAddCustomer.Data.Thana1 = "616";
-            aAddCustomer.Data.Thana2 = "632";
-            aAddCustomer.Data.CityCode1 = "006";
-            aAddCustomer.Data.CityCode = "026";
-            aAddCustomer.Data.StateCode1 = "010";
-            aAddCustomer.Data.State = "030";
-            aAddCustomer.Data.BussPhnNum = "0188888888888";
-            aAddCustomer.Data.CountryCode = "BD";
-            aAddCustomer.Data.CountryCode2 = "BD";
-            aAddCustomer.Data.NrstLndMark = "Mohammodpur";
-            aAddCustomer.Data.NameAsProof = "NameAsProof";
-            aAddCustomer.Data.IsTheCust = "N";
-            aAddCustomer.Data.PolitclEP = "10";
-            aAddCustomer.Data.MnthlyIncome = "150000";
-            aAddCustomer.Data.CustTyp = "01";
-            aAddCustomer.Data.TitleCode = "02";
-            aAddCustomer.Data.CustFirstName = "Tazul";
-            aAddCustomer.Data.Addr11 = "Address 11";
-            aAddCustomer.Data.Addr12 = "Address 12";
-            aAddCustomer.Data.Addr10 = "Address 10 ";
-            aAddCustomer.Data.Addr9 = "Address 9 need to test";
-            aAddCustomer.Data.Addr1 = "Address 1";
-            aAddCustomer.Data.Addr2 = "Address 2";
-            aAddCustomer.Data.CountryCode1 = "BD";
-            aAddCustomer.Data.Natlty = "BD";
-            aAddCustomer.Data.AcctNum1 = "113039058";
-            aAddCustomer.Data.IntroName = "Mr WAHIDUZZAMAN";
-            aAddCustomer.Data.Relcusm = "06";
-            aAddCustomer.Data.NumAcct = "1130310293845";
-            aAddCustomer.Data.MobileNum235 = "01712671832";
-            aAddCustomer.Data.AcctBrnch = "00113";
-            aAddCustomer.Data.LangCode = "01";
-            aAddCustomer.Data.PpVrfy = "N";
-            aAddCustomer.Data.IdNum1 = "5955963891";
-            aAddCustomer.Data.IdTyp1 = "01";
-            aAddCustomer.Data.IdExpryDt = "";
-            aAddCustomer.Data.DmstcRisk = "ZZ";
-            aAddCustomer.Data.CrossBrdrRisk = "ZZ";
-            aAddCustomer.Data.SegCode = "0001";
-            aAddCustomer.Data.HomeBrchNum = "113";
-            aAddCustomer.Data.CountryRisk = "02";
-            aAddCustomer.Data.GrpCode = "ZZ";
-            aAddCustomer.Data.FatherName = "Farid Ahmad";
-            aAddCustomer.Data.MotherMdnName = "Nazmun nahar";
-            aAddCustomer.Data.DtOfBirth = "25011994";
-            aAddCustomer.Data.ResdncyCode = "1";
-            aAddCustomer.Data.OrigCode = "ZZ";
-            aAddCustomer.Data.StrtCode = "ZZ";
-            aAddCustomer.Data.GndrCode = "M";
-            aAddCustomer.Data.CustGrp = "0103";
-            aAddCustomer.Data.CountryOfRsdnc = "BD";
-            aAddCustomer.Data.RevaltnFlag = "N";
-            aAddCustomer.Data.OccupCode = "F";
-            aAddCustomer.Data.PrevOccupCode = "0025";
-            aAddCustomer.Data.IncomeRange = "01";
-            aAddCustomer.Data.CustRiskImp = "3";
-            aAddCustomer.Data.CountryOfBirth = "BD";
-            aAddCustomer.Data.CountryOfTaxRsdnc = "BD";
-            aAddCustomer.Data.CounRiskImp = "3";
-            aAddCustomer.Data.CaptureProprietor = "N";
-            aAddCustomer.Data.PrimTyp1 = "04";
-            aAddCustomer.Data.SecndTyp1 = "03";
-            aAddCustomer.Data.PrimTyp2 = "915001";
-            aAddCustomer.Data.DisttBirth = "006";
-            aAddCustomer.Data.SmeTyp = "00";
-            aAddCustomer.Data.BankNm = "123";
-            aAddCustomer.Data.CorrAddrLan = "+88";
-            aAddCustomer.Data.CorrAddrMobile = "+88";
-            aAddCustomer.Data.EmergencyContactMobile = "+88";
-            #endregion
+
+
+
+            #region Comment TCS
+            //var propertiesRqHeader = typeof(RqHeader_Type_Custom).GetProperties();
+
+            //foreach (PropertyInfo property in propertiesRqHeader)
+            //{
+            //    var propValue = property.GetValue(aAddCustomer.RqHeader, null);
+            //    if (propValue != null && !string.IsNullOrWhiteSpace(propValue.ToString()))
+            //        rqHeader.AppendChild(doc.CreateElement(string.IsNullOrWhiteSpace(prefix) ? "" : prefix, property.Name, v2)).InnerText = propValue == null ? "" : propValue.ToString();
+            //}
+            //var propertiesData = typeof(CustAddRqData_Type_Custom).GetProperties();
+            //foreach (PropertyInfo property in propertiesData)
+            //{
+            //    var propValue = property.GetValue(aAddCustomer.Data, null);
+            //    if (propValue != null && !string.IsNullOrWhiteSpace(propValue.ToString()))
+            //        data.AppendChild(doc.CreateElement(string.IsNullOrWhiteSpace(prefix) ? "" : prefix, property.Name, v2)).InnerText = propValue == null ? "" : propValue.ToString();
+            //}
+            //var generatedXML = doc.GetOuterXml();
 
             #endregion
-
-            var propertiesRqHeader = typeof(RqHeader_Type_Custom).GetProperties();
-
-            foreach (PropertyInfo property in propertiesRqHeader)
-            {
-                var propValue = property.GetValue(aAddCustomer.RqHeader, null);
-                if (propValue != null && !string.IsNullOrWhiteSpace(propValue.ToString()))
-                    rqHeader.AppendChild(doc.CreateElement(string.IsNullOrWhiteSpace(prefix) ? "" : prefix, property.Name, v2)).InnerText = propValue == null ? "" : propValue.ToString();
-            }
-            var propertiesData = typeof(CustAddRqData_Type_Custom).GetProperties();
-            foreach (PropertyInfo property in propertiesData)
-            {
-                var propValue = property.GetValue(aAddCustomer.Data, null);
-                if (propValue != null && !string.IsNullOrWhiteSpace(propValue.ToString()))
-                    data.AppendChild(doc.CreateElement(string.IsNullOrWhiteSpace(prefix) ? "" : prefix, property.Name, v2)).InnerText = propValue == null ? "" : propValue.ToString();
-            }
-            var generatedXML = doc.GetOuterXml();
 
             Console.ReadKey();
         }
+
     }
 }
-
